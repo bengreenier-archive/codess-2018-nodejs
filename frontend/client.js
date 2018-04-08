95c5e6b080ed558c31b2470044d91ea926fc08c2
bengreenier <ben@bengreenier.com>
2018-04-08 09:16:26 -0400

    basic frontend working

--- frontend/client.js
+++ frontend/client.js
@@ -9,12 +9,46 @@ let boundDomElement = new Ractive({
   }
 })
 
+boundDomElement.on('delete', (context, messageId) => {
+  superagent
+    .delete('/messages/' + messageId)
+    .then((res) => {
+      if (res.ok) {
+        const messages = boundDomElement.get('messages')
+        boundDomElement.set('messages', messages.filter(m => m.id !== messageId))
+      }
+    })
+})
+
+boundDomElement.on('post', (context) => {
+  // use dom navigation (this is semi-brittle) to find the values
+  const usernameNode = context.node.previousElementSibling.previousElementSibling
+  const contentsNode = context.node.previousElementSibling
+  const username = usernameNode.value
+  const contents = contentsNode.value
+
+  superagent
+    .post('/messages')
+    .send({username, contents})
+    .then((res) => {
+      if (res.ok) {
+        usernameNode.value = ''
+        contentsNode.value = ''
+
+        const messages = boundDomElement.get('messages')
+        messages.push(res.body)
+        boundDomElement.set('messages', messages)
+      }
+    })
+})
+
 function updateIfNeeded () {
-  superagent('/messages').then((res) => {
-    if (res.ok) {
-      boundDomElement.set('messages', res.body)
-    }
-  })
+  superagent('/messages')
+    .then((res) => {
+      if (res.ok) {
+        boundDomElement.set('messages', res.body)
+      }
+    })
 }
 
 setInterval(updateIfNeeded, 5000)
